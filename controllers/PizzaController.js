e19a40889a86963625557ee29fd1e43b6f6d139f
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-02-24 20:16:56 -0800

    move food item logic to factory and add ability to compute total.

--- controllers/PizzaController.js
+++ controllers/PizzaController.js
@@ -1,15 +1,6 @@
 foodShop.controller('PizzaCtrl', function PizzaCtrl($scope, FoodFactory) {
-  $scope.pizzaItems = [];
-  $scope.pizzaOptions = [
-  { name: "Pepperoni", price: 10.50 },
-  { name: "Cheese", price: 8.50 },
-  { name: "Chicken Garlic", price: 13.50 },
-  { name: "Pineapple", price: 13.50 },
-  { name: "Glutenfree", price: 12.50},
-  ];
-
-  $scope.addPizza = function(item) {
-    $scope.pizzaItems.push(item)
-    console.log($scope.pizzaItems);
-  };
+  $scope.pizzaOptions = FoodFactory.pizzaOptions;
+  $scope.pizzaItems = FoodFactory.pizzaItems;
+  $scope.FoodFactory = FoodFactory;
+  $scope.pizzaTotal = FoodFactory.getPizzaTotal;
 });
--- services/FoodFactory.js
+++ services/FoodFactory.js
@@ -1,4 +1,51 @@
 foodShop.factory('FoodFactory', function FoodFactory() {
  var factory = {};
+
+ factory.iceCreamItems = [];
+ var iceTotal = null;
+ factory.pizzaItems = [];
+ var pizzaTotal = null;
+
+ factory.iceCreamOptions = [
+ { name: "Chocolate", price: 2.49 },
+ { name: "Cookies & Cream", price: 3.59 },
+ { name: "Rocky Road", price: 2.29 },
+ { name: "Mint", price: 2.19 },
+ { name: "Vanilla", price: 2.29},
+ ];
+
+ factory.pizzaOptions = [
+ { name: "Pepperoni", price: 10.50 },
+ { name: "Cheese", price: 8.50 },
+ { name: "Chicken Garlic", price: 13.50 },
+ { name: "Pineapple", price: 13.50 },
+ { name: "Glutenfree", price: 12.50},
+ ];
+
+ factory.addPizza = function(item) {
+   factory.pizzaItems.push(item);
+   pizzaTotal += factory.pizzaItems[0].price;
+ };
+
+ factory.addIceCream = function(item) {
+   factory.iceCreamItems.push(item);
+   iceTotal += factory.iceCreamItems[0].price;
+ };
+
+ factory.getIceTotal = function() {
+   if (iceTotal === null) {
+     return 0
+   } else {
+     return iceTotal.toFixed(2);
+   }
+ };
+
+ factory.getPizzaTotal = function() {
+   if (pizzaTotal === null) {
+     return 0
+   } else {
+     return pizzaTotal.toFixed(2);
+   }
+ };
  return factory;
 });
